ef0e1124dc6bb42ff32895a90c7659efdf6a4add
Ordorbin <ordorbin13@gmail.com>
2025-04-25 10:35:51 +0300

    Update app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -127,7 +127,9 @@ def dashboard():
 
 
 if __name__ == '__main__':
-    print("🚀 Connecting to MySQL...")
+    print("🚀 Connecting to MySQL....")
     create_table()  # Ensure table exists before running Flask
     print("🚀 Running Flask on http://127.0.0.1:5000/")
     app.run(debug=True)
+
+# testing
